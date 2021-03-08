65841507751276e2394671c136f961614fa4502f
Milosz Wilczynski <mwilcz08@gmail.com>
2021-03-08 12:08:12 +0100

    end

--- cms_backend/src/server.js
+++ cms_backend/src/server.js
@@ -1,15 +1,16 @@
-const app = require('./app');
-const db = require('./models');
+import app from './app';
+import db from './models';
 
 app.set('port', process.env.NODE_PORT || 8080);
 
-const server = app.listen(app.get('port'), () => {
+const server = app.listen(app.get('port'), async () => {
   console.log(`Server is listening on port ${app.get('port')}`);
 });
 
 db.sequelize
   //.sync({ force: true }) //deletes and creates tables without seeding
-  .sync({ logging: console.log }) //only sync without deleting tables etc.
+  //.sync({ logging: console.log }) //only sync without deleting tables etc.
+  .sync()
   .then(() => {
     console.log('Database successfully connected...');
   })
